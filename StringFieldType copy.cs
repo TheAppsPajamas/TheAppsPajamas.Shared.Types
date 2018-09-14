5570b833e167db09343a60ef861c07ce49869d72
Dean Marcussen <dean@theappspajamas.com>
2018-09-14 07:29:18 +0100

    test

---
+++ StringFieldType copy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheAppsPajamas.Shared.Types
+{
+    public class StringFieldType : FieldType
+    {
+        private StringFieldDisplayType _fieldDisplayType;
+        public StringFieldDisplayType FieldDisplayType { get { return _fieldDisplayType; } }
+        
+        private string _defaultValue;
+        public string DefaultValue {  get { return _defaultValue; } }
+
+        public StringFieldType(int value
+            , string displayName
+            , ProjectType projectType
+            , FieldHolderType fieldHolderType
+            , int order
+            , bool isForClient
+            , bool isProdReady
+            , bool defaultToDisabled
+            , StringFieldType inheritsFromDefault
+            , StringFieldDisplayType fieldDisplayType
+            , string defaultValue) : base(value, displayName, projectType, fieldHolderType, order, isForClient, isProdReady, defaultToDisabled, inheritsFromDefault)
+        {
+            _fieldDisplayType = fieldDisplayType;
+            _defaultValue = defaultValue;
+        }
+
+    }
+}
