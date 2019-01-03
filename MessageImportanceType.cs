84fef233e80ff28c4a309e01fed86c4f3af2311d
Dean Marcussen <dean@theappspajamas.com>
2019-01-03 19:48:08 +0000

    Migrated MessageImportanceType to shared types

---
+++ MessageImportanceType.cs
@@ -0,0 +1,21 @@
+﻿using TheAppsPajamas.Shared.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheAppsPajamas.Shared.Types
+{
+    public class MessageImportanceType : Enumeration<MessageImportanceType>
+    {
+        public static readonly MessageImportanceType Debug = new MessageImportanceType(0, "Debug");
+        public static readonly MessageImportanceType Success = new MessageImportanceType(1, "Success");
+        public static readonly MessageImportanceType Information = new MessageImportanceType(2, "Information");
+        public static readonly MessageImportanceType Warning = new MessageImportanceType(3, "Warning");
+        public static readonly MessageImportanceType Error = new MessageImportanceType(4, "Error");
+
+
+        protected MessageImportanceType(int value, string displayName) : base(value, displayName)
+        {
+        }
+    }
+}
